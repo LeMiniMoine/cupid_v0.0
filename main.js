3d4f3fab76f316b99fca18171022c4b9f67ca4e6
Mike Dauphinais <mdauphinais@voxbone.com>
2016-04-09 17:14:33 +0200

    File manager

--- main.js
+++ main.js
@@ -10,101 +10,72 @@ var simpleGit = require('simple-git')();
 var Client = require('node-rest-client').Client;
 var express = require('express');
 var restApp = express();
-
+var ngrok = require('ngrok');
+var bodyParser = require('body-parser');
+var lodash = require('lodash');
 
 var user = 'mike';
-var changeList = [];
+var auth = 'Bearer N2I5YzMxMDMtMTQ5NS00N2MwLThkYzItZjU4OWQ3YmFhZjMwNzVmNDljMzYtYzc3';
 var roomId = 'Y2lzY29zcGFyazovL3VzL1JPT00vMDA2ZTdiMjAtZmQxMS0xMWU1LTg2NDUtOWZjNGE1MmNmMGQ5';
 var client = new Client();
 
-restApp.get('/', function (req, res) {
-    res.send('Hello Seattle\n');
-});
-restApp.listen(3001);
-console.log('Listening on port 3001...');
+var teamChanges = [];
 
+var args = {
+    headers: {
+        'Content-type': 'application/json; charset=utf-8',
+        'Authorization': auth
+    }
+};
 
-chokidar.watch('.', {ignored: '.idea'}).on('change', function (path, event) {
-    //console.log('Change in file: ' + path);
-    simpleGit.diffSummary(function (error, response) {
-        //console.log(response);
-        var changedFiles = response.files;
-        /*        changedFiles = response.files;
-         response.files.forEach(function (element) {
-         changedFiles[element.index] = element;
-         });*/
-        //console.log('Changed files: ', changedFiles);
-        //console.log('Stringified response: ', JSON.stringify(response));
-        //console.log('Stringified changes: ', JSON.stringify(changedFiles));
-
-        //console.log(changedFiles);
-        var changes = {
-            user: user,
-            files: changedFiles
+ngrok.connect(
+    {
+        proto: 'http',
+        addr: 3033
+    },
+    function (err, url) {
+        args.data = {
+            name: 'mikeWebHook',
+            targetUrl: url,
+            resource: 'messages',
+            event: 'created',
+            filter: 'roomId='.concat(roomId)
         };
-        //console.log(changes);
-        var stringChanges = JSON.stringify(changes);
-        //console.log(stringChanges);
-        var args = {
-            headers: {
-                "Content-type": "application/json; charset=utf-8",
-                "Authorization": "Bearer N2I5YzMxMDMtMTQ5NS00N2MwLThkYzItZjU4OWQ3YmFhZjMwNzVmNDljMzYtYzc3"
-            },
-            data: {
-                "roomId": "Y2lzY29zcGFyazovL3VzL1JPT00vMDA2ZTdiMjAtZmQxMS0xMWU1LTg2NDUtOWZjNGE1MmNmMGQ5",
-                "text": stringChanges
-            }
+        client.post('https://api.ciscospark.com/v1/webhooks', args, function (data, response) {
+            console.log('Webhook created');
 
-        };
-        client.post('https://api.ciscospark.com/v1/messages', args, function (data, response) {
-            // parsed response body as js object
-            console.log(data);
-            // raw response
-            //console.log(response);
+            restApp.use( bodyParser.json() );
+            restApp.post('/', function (req, res) {
+                getMessageFromCisco(req.body.data.id);
+            });
+            restApp.listen(3033);
+            console.log('Listening on port 3033...');
         });
     });
 
-
-    //changeList.push(change);
-    //console.log(changeList);
-
-
-
-    /*    client.get("https://api.ciscospark.com/v1/rooms", args, function (data, response) {
-     // parsed response body as js object
-     console.log(data);
-
-
-     // raw response
-     //console.log(response);
-     });*/
-
-    /*    client.post('https://api.ciscospark.com/v1/messages', args, function (data, response) {
-     // parsed response body as js object
-     console.log(data);
-     // raw response
-     //console.log(response);
-     });*/
-
-    //console.log(event);
+chokidar.watch('.', {ignored: '.idea'}).on('change', function (path, event) {
     simpleGit.diffSummary(function (error, response) {
-        //console.log(error);
-        //console.log(response);
-    });
 
-    //console.log('Chosen output: ', gitDiff);
-    //console.log('Full output: ', simpleGit.diff());
+        var changes = {
+            user: user,
+            files: response.files
+        };
+
+        if(shouldSendNewChanges(changes)) {
+            console.log('Sending new changes...');
+            args.data = {
+                'roomId': roomId,
+                'text': JSON.stringify(changes)
+            };
+
+            client.post('https://api.ciscospark.com/v1/messages', args, function (data, response) {
+
+            });
+        }
+    });
 });
 
-
-/*chokidar.watch('./.git').on('change', function (path, event) {
- console.log('Change in git: ', event);
- });*/
-
 var mainWindow = null;
-
-
-//var log = console.log.bind(console);
 
 app.on('ready', function () {
     mainWindow = new BrowserWindow({
@@ -117,3 +88,69 @@ app.on('ready', function () {
 
     mainWindow.loadURL('file://' + __dirname + '/app/index.html');
 });
+
+var getMessageFromCisco = function (messageId) {
+    client.get('https://api.ciscospark.com/v1/messages/'.concat(messageId), args, function(data, response) {
+        var receivedChanges = JSON.parse(data.text);
+        console.log(receivedChanges);
+        updateTeamChangeList(receivedChanges);
+    })
+};
+
+var shouldSendNewChanges = function(changes) {
+    var myPreviousChangeList = teamChanges.filter(function (change) {
+        return change.user == user;
+    }).map(function (change) {return change.file});
+
+    var myCurrentChangeList = changes.files.map(function(element) {return {file:element.file}});
+
+    var fileChangesToAdd = getObjectsPresentInArray1NotInArray2(myCurrentChangeList, myPreviousChangeList);
+
+    fileChangesToAdd.forEach(function(file) {
+        teamChanges.push({user: user, file: file});
+    });
+
+    var fileChangesToRemove = getObjectsPresentInArray1NotInArray2(myPreviousChangeList, myCurrentChangeList);
+
+    fileChangesToRemove.forEach(function(file) {
+        teamChanges.slice(teamChanges.indexOf({user: user, file: file}));
+    });
+
+    console.log('Should send changes: ', (!lodash.isEmpty(fileChangesToAdd) || !lodash.isEmpty(fileChangesToRemove)));
+
+    return (!lodash.isEmpty(fileChangesToAdd) || !lodash.isEmpty(fileChangesToRemove));
+};
+
+var getObjectsPresentInArray1NotInArray2 = function(array1, array2) {
+    var bIds = {};
+    array2.forEach(function(obj){
+        bIds[obj.id] = obj;
+    });
+    return array1.filter(function(obj){
+        return !(obj.id in bIds);
+    });
+};
+
+var updateTeamChangeList = function (message) {
+    var remoteUser = message.user;
+    var previousChangeListFromUser = teamChanges.filter(function (change) {
+        return change.user == remoteUser;
+    }).map(function (change) {return change.file});
+
+    var currentChangeListForUser = message.files.map(function(element) {return {file:element.file}});
+
+    var fileChangesToAdd = getObjectsPresentInArray1NotInArray2(currentChangeListForUser, previousChangeListFromUser);
+
+    fileChangesToAdd.forEach(function(file) {
+        teamChanges.push({user: user, file: file});
+    });
+
+    var fileChangesToRemove = getObjectsPresentInArray1NotInArray2(previousChangeListFromUser, currentChangeListForUser);
+
+    fileChangesToRemove.forEach(function(file) {
+        teamChanges.slice(teamChanges.indexOf({user: user, file: file}));
+    });
+
+    console.log('Team change list updated: ', (!lodash.isEmpty(fileChangesToAdd) || !lodash.isEmpty(fileChangesToRemove)));
+};
+
